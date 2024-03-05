d28508ad1a5dd56cc69154eb3d331fd7814c2a30
xCodeZero <42394349+xCodeZero@users.noreply.github.com>
2024-03-05 11:52:11 +0800

    added stripe

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,7 +1,7 @@
 /* eslint-disable camelcase */
-// import { createTransaction } from "@/lib/actions/transaction.action";
+import { createTransaction } from "@/lib/actions/transaction.action";
 import { NextResponse } from "next/server";
-// import stripe from "stripe";
+import stripe from "stripe";
 
 export async function POST(request: Request) {
   const body = await request.text();
@@ -12,31 +12,31 @@ export async function POST(request: Request) {
   let event;
 
   try {
-    // event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
   } catch (err) {
     return NextResponse.json({ message: "Webhook error", error: err });
   }
 
   // Get the ID and type
-  // const eventType = event.type;
+  const eventType = event.type;
 
   // CREATE
-  // if (eventType === "checkout.session.completed") {
-  //   const { id, amount_total, metadata } = event.data.object;
+  if (eventType === "checkout.session.completed") {
+    const { id, amount_total, metadata } = event.data.object;
 
-  //   const transaction = {
-  //     stripeId: id,
-  //     amount: amount_total ? amount_total / 100 : 0,
-  //     plan: metadata?.plan || "",
-  //     credits: Number(metadata?.credits) || 0,
-  //     buyerId: metadata?.buyerId || "",
-  //     createdAt: new Date(),
-  //   };
+    const transaction = {
+      stripeId: id,
+      amount: amount_total ? amount_total / 100 : 0,
+      plan: metadata?.plan || "",
+      credits: Number(metadata?.credits) || 0,
+      buyerId: metadata?.buyerId || "",
+      createdAt: new Date(),
+    };
 
-  //   // const newTransaction = await createTransaction(transaction);
-
-  //   return NextResponse.json({ message: "OK", transaction: newTransaction });
-  // }
+    const newTransaction = await createTransaction(transaction);
+    
+    return NextResponse.json({ message: "OK", transaction: newTransaction });
+  }
 
   return new Response("", { status: 200 });
 }
