5027bf7d497563c3c4973d51e52fce7408a1df10
xCodeZero <42394349+xCodeZero@users.noreply.github.com>
2024-03-09 17:55:27 +0800

    added gcash payments 3

--- app/(root)/paymentdetails/page.tsx
+++ app/(root)/paymentdetails/page.tsx
@@ -4,29 +4,34 @@ import { getUserById } from "@/lib/actions/user.actions";
 import { getUserPayments } from "@/lib/actions/payments.action";
 import { PaymentCollection } from "@/components/shared/PaymentCollection";
 
-const Gcash = async ({ searchParams }: SearchParamProps) => {
-  const { userId } = auth();
-  if (!userId) redirect("/sign-in");
-  const user = await getUserById(userId);
+const PaymentDetails = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+
   const payments = await getUserPayments({ page, userId: user._id });
 
   return (
-    <div>
-      <section className="home">
-        <h1 className="home-heading">
-          Unleash Your Creative Vision with AIditor
-        </h1>
-      </section>
-      <section className="mt-8 md:mt-14">
-        <PaymentCollection
-          payments={payments?.data}
-          totalPages={payments?.totalPages}
-          page={page}
-        />
-      </section>
-    </div>
+    <>
+      <div>
+        <section className="home">
+          <h1 className="home-heading">
+            Unleash Your Creative Vision with AIditor
+          </h1>
+        </section>
+        <section className="mt-8 md:mt-14">
+          <PaymentCollection
+            payments={payments?.data}
+            totalPages={payments?.totalPages}
+            page={page}
+          />
+        </section>
+      </div>
+    </>
   );
 };
 
-export default Gcash;
+export default PaymentDetails;
