4db32bd6d828d3de0fe2431e7ad599edf2870ae5
xCodeZero <42394349+xCodeZero@users.noreply.github.com>
2024-03-09 17:59:14 +0800

    added gcash payments 4

--- app/(root)/paymentdetails/page.tsx
+++ app/(root)/paymentdetails/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import { getUserById } from "@/lib/actions/user.actions";
 import { getUserPayments } from "@/lib/actions/payments.action";
 import { PaymentCollection } from "@/components/shared/PaymentCollection";
+import Header from "@/components/shared/Header";
 
 const PaymentDetails = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
@@ -16,20 +17,15 @@ const PaymentDetails = async ({ searchParams }: SearchParamProps) => {
 
   return (
     <>
-      <div>
-        <section className="home">
-          <h1 className="home-heading">
-            Unleash Your Creative Vision with AIditor
-          </h1>
-        </section>
-        <section className="mt-8 md:mt-14">
-          <PaymentCollection
-            payments={payments?.data}
-            totalPages={payments?.totalPages}
-            page={page}
-          />
-        </section>
-      </div>
+      <Header title="Payments" />
+
+      <section className="mt-8 md:mt-14">
+        <PaymentCollection
+          payments={payments?.data}
+          totalPages={payments?.totalPages}
+          page={page}
+        />
+      </section>
     </>
   );
 };
